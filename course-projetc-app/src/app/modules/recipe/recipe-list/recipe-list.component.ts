4466d900fab6130395461e464ba2415adb13bea0
Gabriel Ataide <gabriel.ataide@enforcegroup.com.br>
2021-11-14 17:44:55 -0300

    shopping list improvements

--- course-projetc-app/src/app/modules/recipe/recipe-list/recipe-list.component.ts
+++ course-projetc-app/src/app/modules/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('tst', 'tst desc', 'https://c.pxhere.com/images/d0/54/15a09b734bfd3e341434c2191a94-1417896.jpg!d'),
+    new Recipe('tst', 'tst desc', 'https://c.pxhere.com/images/d0/54/15a09b734bfd3e341434c2191a94-1417896.jpg!d'),
+    new Recipe('tst', 'tst desc', 'https://c.pxhere.com/images/d0/54/15a09b734bfd3e341434c2191a94-1417896.jpg!d')
+  ];
+
+  constructor () {}
 }
--- course-projetc-app/src/app/modules/shopping/shopping-list/shopping-list.component.ts
+++ course-projetc-app/src/app/modules/shopping/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  constructor () {}
 }
